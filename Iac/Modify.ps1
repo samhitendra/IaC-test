1dcf96da0a09faed310e478d0204116a4d3c0231
Shambhavi <34913088+samhitendra@users.noreply.github.com>
2018-02-15 10:53:17 +0530

    Update Modify.ps1

--- Iac/Modify.ps1
+++ Iac/Modify.ps1
@@ -7,7 +7,7 @@ $VMs=Get-VM | Where-Object { $_.Name -like '*b6-IaC-Win*'}
 
 Get-VMGuestNetworkInterface $VMs |
 Where-Object { $_.ip -ne $null} |
-//Set-vmguestnetworkinterface -ip “192.168.15.120” -netmask “255.255.255.0” -gateway “192.168.15.2” | Out-Null
+#Set-vmguestnetworkinterface -ip “192.168.15.120” -netmask “255.255.255.0” -gateway “192.168.15.2” | Out-Null
 
 Set-VM $VMs -NumCPU 4  -confirm:$false | Out-Null 
 Set-VM $VMs -MemoryGB 3  -confirm:$false | Out-Null 
