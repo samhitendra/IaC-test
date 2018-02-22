961ae4d294cab91b706d8937d8545762d4570cc8
Shambhavi <34913088+samhitendra@users.noreply.github.com>
2018-02-22 15:03:21 +0530

    Update Modify.ps1

--- Iac/Modify.ps1
+++ Iac/Modify.ps1
@@ -6,7 +6,7 @@ Connect-VIServer b6-vc-65.cloud.ucs -User 'shambhavi@cloud.ucs' -Password 'Acc12
 $VMs=Get-VM | Where-Object { $_.Name -like "*b6-IaC-lin*" -or $_.Name -like "*b6-IaC-win*"}
 
 Set-VM $VMs -NumCPU 3  -confirm:$false | Out-Null 
-Set-VM $VMs -MemoryGB 4  -confirm:$false | Out-Null 
+Set-VM $VMs -MemoryGB 3  -confirm:$false | Out-Null 
 Get-VM $VMs | New-HardDisk -CapacityGB 1 -StorageFormat Thin -Persistence persistent | Out-Null 
 
 Disconnect-VIServer b6-vc-65.cloud.ucs -Confirm:$false
