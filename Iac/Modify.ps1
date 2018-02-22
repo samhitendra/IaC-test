4fc2242e8a3c2bdcc876e9c30da7b376e250f8d9
Shambhavi <34913088+samhitendra@users.noreply.github.com>
2018-02-22 14:58:35 +0530

    Update Modify.ps1

--- Iac/Modify.ps1
+++ Iac/Modify.ps1
@@ -1,10 +1,12 @@
 Import-Module ServerManager
 add-pssnapin VMware.VimAutomation.Core
 
-Connect-VIServer b6-vc-65.cloud.ucs
+Connect-VIServer b6-vc-65.cloud.ucs -User 'shambhavi@cloud.ucs' -Password 'Acc1234$$'
 
 $VMs=Get-VM | Where-Object { $_.Name -like "*b6-IaC-lin*" -or $_.Name -like "*b6-IaC-win*"}
 
 Set-VM $VMs -NumCPU 3  -confirm:$false | Out-Null 
 Set-VM $VMs -MemoryGB 4  -confirm:$false | Out-Null 
 Get-VM $VMs | New-HardDisk -CapacityGB 1 -StorageFormat Thin -Persistence persistent | Out-Null 
+
+Disconnect-VIServer b6-vc-65.cloud.ucs -Confirm:$false
